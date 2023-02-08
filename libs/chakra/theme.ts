994ac7a8f1393b262ee5c026aaf0dc14f5d9e8eb
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-02-08 18:49:02 +0700

    feat(chakra): theme GhostProject/redditc#7
    + libs/chakra/theme.ts
      pages/_app.tsx
    + Create new libs chakara `theme` for extend the theme. Extending the
      theme in chakra provider in `_app`
    + This commit enable extend theme with chakra provider.

---
+++ libs/chakra/theme.ts
@@ -0,0 +1,49 @@
+import { extendTheme, StyleConfig, ThemeConfig } from "@chakra-ui/react";
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/700.css"
+
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: true,
+};
+
+const styles = {
+  global: () => ({
+    body: {
+      bg: "gray.200",
+      color: 'black'
+    }
+  })
+}
+
+const components = {
+  Button: {
+    // variant: {
+    //   ""
+    // }
+  }
+}
+
+const colors = {
+  brand: {
+    100: "#FF1c00"
+  }
+}
+
+const fonts = {
+  body: "Open Sans, sans-serif"
+}
+
+
+
+const theme = extendTheme({
+  config,
+  styles,
+  components,
+  fonts,
+  colors
+})
+
+export default theme
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+import Layout from '../components/layouts/main/main'
+import theme from '../libs/chakra/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
