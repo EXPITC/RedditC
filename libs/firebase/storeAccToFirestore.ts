8bb8444962d8ecd515597a187822864e2b7bed2e
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-02-12 14:16:05 +0700

    ref(firestore): uid users
    + components/modals/authModal/authForm/Signup.tsx
      libs/firebase/storeAccToFirestore.ts
    + This changes id generated by `add` and change to adopt `set` custom
      id.
    + This commit changes id in users fire store.

--- libs/firebase/storeAccToFirestore.ts
+++ libs/firebase/storeAccToFirestore.ts
@@ -1,17 +1,19 @@
 import { User } from 'firebase/auth'
-import { addDoc, collection, doc, setDoc } from 'firebase/firestore'
+import { doc, setDoc } from 'firebase/firestore'
 import { firestore } from './clientApp'
 
-const storeAccToFirestore = async (user: User, signup: boolean = false) => {
+const storeAccToFirestore = async (user: User) => {
   const userCredential = JSON.parse(JSON.stringify(user))
 
-  if (signup)
-    return await addDoc(collection(firestore, "users"), userCredential)
+  // i do leave this uncomment
+  // await addDoc(collection(firestore, "users", user.uid), userCredential)
 
   // Others else OAuth provider
   // I set to setDoc beacuse username of the user can change base on what their provider
   // Note: set it like put, and addDoc is more like patch
   // well it can be use set for all but that not the meaning.
+  // because add will auto set generated id and when we use create 
+  // community addin cummunity subs to user id then I decide to use custom id by default user.uid use Set.
   const docRef = doc(firestore, "users", user.uid)
   await setDoc(docRef, userCredential)
 }
