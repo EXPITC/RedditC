e5b360b3ba5848b2e627d93280ae703876c69445
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-02-13 14:34:24 +0700

    feat(navbar): fix responsiveness
    + components/layouts/main/
        -|navbar.tsx
        -|navbar/logo.tsx

--- components/layouts/main/navbar/logo.tsx
+++ components/layouts/main/navbar/logo.tsx
@@ -4,7 +4,7 @@ import styles from '@/styles/Logo.module.css'
 
 
 const Logo = () => (
-  <Flex align="center">
+  <Flex align="center" minWidth="fit-content">
     <Image src="/images/redditFace.svg" alt="redditFace" height={32} width={32} priority />
     <Image src="/images/redditText.svg" alt="redditText" height={43} width={73} className={styles.logoText} priority />
   </Flex>
