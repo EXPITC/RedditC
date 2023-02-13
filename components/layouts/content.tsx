1f6ad8a21b216b5b8f2d4cd3aefa692be4051675
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-02-13 16:11:23 +0700

    feat(layouts): content
    + components/layouts/content.tsx
      pages/r/[communityID]/index.tsx
    + Create content layouts that take 2 children to render. Test integrate
      to r `index`.
    + This commit enable content to have consistent layouts with content in
      the middle and info the right when hit md to bottom right info will be
      hide content take over space available.

---
+++ components/layouts/content.tsx
@@ -0,0 +1,40 @@
+import { Flex } from "@chakra-ui/react"
+
+interface props {
+  children: React.ReactNode
+}
+
+const ContentLayouts = ({ children }: props) => (
+
+  // If children not exist its not render
+  children ?
+    <Flex p="16px 0" justify="center">
+      <Flex
+        width="95%"
+        maxWidth="860px"
+        justify="center"
+      >
+        {/* Mid content */}
+        <Flex
+          direction="column"
+          width={['100%', '65%']}
+          mr={["0", "6"]}
+        >
+          {children[0 as keyof typeof children]}
+        </Flex>
+
+        {/* Right Info */}
+        <Flex
+          direction="column"
+          flexGrow="1"
+          display={['none', 'none', 'none', 'flex']}
+        >
+          {children[1 as keyof typeof children]}
+        </Flex>
+      </Flex>
+    </Flex>
+    :
+    <h3>Whoops no children</h3>
+)
+
+export default ContentLayouts
