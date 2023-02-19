06f64638d725c481693700299a7b4c7fbb69bc23
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-02-19 18:55:51 +0700

    feat(communitySubs): store communityData
    + pages/r/[communityID]/index.tsx
    + After `communityData` retrive set data to `currentCommunity` by
      `setCommunitySubs` use recoil, in first render use `useEffect` react
      life cycle.
    + This commit enable to populate current community data when first
      render.

--- pages/r/[communityID]/index.tsx
+++ pages/r/[communityID]/index.tsx
@@ -3,9 +3,11 @@ import PageR404 from '@/components/r/404'
 import Header from '@/components/r/header'
 import LinkPost from '@/components/r/linkpost'
 import PostTimeline from '@/components/r/postTimeline'
-import { communityData } from '@/libs/atoms/communitiesAtoms'
+import { communityData, communitySubsState } from '@/libs/atoms/communitiesAtoms'
 import getcommunityData from '@/libs/firebase/communityData'
 import { GetServerSideProps } from 'next'
+import { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil'
 
 interface serverProps extends GetServerSideProps {
   params: {
@@ -28,7 +30,14 @@ export const getServerSideProps = async ({
 
 export default function communityPage({ communityData }: { communityData: communityData }) {
   if (!communityData) return <PageR404 />
+  const setCommunitySubs = useSetRecoilState(communitySubsState)
 
+  useEffect(() => {
+    setCommunitySubs(prev => ({
+      ...prev,
+      currentCommunity: communityData
+    }))
+  }, [])
   return (
     <>
       <Header
