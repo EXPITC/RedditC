083cb3ce97f52a8bc281dedf3f122525d8f88b4f
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-03-02 18:04:40 +0700

    fix(rpages): index comments switch from menu
    + pages/r/[communityID]/index.tsx
    + Add validation when first render if there any left data from other
      community then clear `post`, i make when clear post it will auto fetch
      data sync with his condition right now, so it will retrive data that
      he need.
    + This commit fix when ever user selected post and manuver to other
      pages from menu commenity in navbar and other communtiy stack with
      other community.

--- pages/r/[communityID]/index.tsx
+++ pages/r/[communityID]/index.tsx
@@ -5,8 +5,11 @@ import Header from '@/components/r/header'
 import LinkPost from '@/components/r/linkpost'
 import PostTimeline from '@/components/r/postTimeline'
 import { communityData } from '@/libs/atoms/communitiesAtoms'
+import { postState } from '@/libs/atoms/postsAtom'
 import getcommunityData from '@/libs/firebase/communityData'
 import { GetServerSideProps } from 'next'
+import { useEffect } from 'react'
+import { useRecoilState, useResetRecoilState } from 'recoil'
 
 interface serverProps extends GetServerSideProps {
   params: {
@@ -29,6 +32,17 @@ export const getServerSideProps = async ({
 
 export default function communityPage({ communityData }: { communityData: communityData }) {
   if (!communityData) return <PageR404 />
+  const [postStateValue, setPostState] = useRecoilState(postState)
+
+  useEffect(() => {
+    // if there any post that not match with current community request new data.
+    if (!postStateValue.posts.find(post => post.communityId !== communityData.id)) return
+
+    setPostState(prev => ({
+      ...prev,
+      posts: []
+    }))
+  }, [])
 
   return (
     <>
