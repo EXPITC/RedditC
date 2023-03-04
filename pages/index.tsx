a58d05af932939efe5b4b29f21e85bece851be33
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-03-04 15:24:09 +0700

    feat(Homepage): #50 #51 #52
    + libs/
        -|firebase/homeFeed.ts
        -|hooks/useHomeFeed.tsx
      pages/index.tsx
    + Create `getUserHomeFeed` & `getNotUserHomeFeed` in `homeFeed` firebase
      to fetch the related post.
    + Create `populateUserHomeFeed`, `populateNoUserHomeFeed` and
      `getNextHomeFeed` in `useHomeFeed`, the function same with the name
      and create `useEffect` that will populate first init depens on user
      login or not ,or login but have subs or not.
    + `index` call `usePost` for handle the vote, delete etc and lifecycle
      transition between selectedPost page/comments or from community back
      to homefeed or vice versa its control the `postState` which will
      influcence the fetching, it also call `useHomeFeed` for fetch the
      requested data depens on the condition need also create ref for fetch
      when hit bottom and apply pagination, the rest just will loop render
      the post from `postStateValue` from `useHomeFeed` on the `Post` and
      small detail about loading with skeleton.
    + This commit enable the home page and will show user the feed or
      postTimeLine depens on their condition weather user login or not or
      login but not having subs Close #51, Close #52.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,41 @@
+import ContentLayouts from "@/components/layouts/content";
+import LinkPost from "@/components/r/linkpost";
+import Post from "@/components/r/postTimeline/post";
+import PostsSkeleton from "@/components/skeleton/postSkeleton";
+import useHomeFeed from "@/libs/hooks/useHomeFeed";
+import usePost from "@/libs/hooks/usePosts";
+import { Stack, Box } from "@chakra-ui/react";
+import { useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+
 export default function Home() {
-  return <h1>Hello word!</h1>
+  const { postStateValue, loading, getNextHomeFeed, user } = useHomeFeed()
+  const postFunction = usePost()
+
+  const ref = useRef<HTMLDivElement>(null)
+  const inView = useInView(ref)
+
+  useEffect(() => {
+    if (loading) return
+    if (postStateValue.totalCollections === -1) return
+
+    if (inView) getNextHomeFeed()
+  }, [inView, postStateValue.totalCollections, loading])
+
+  return (
+    <ContentLayouts>
+      <>
+        <LinkPost />
+        <Stack mt="10px">
+          {postStateValue.posts.map(post => <Post key={post.id} homeFeed
+            isUserCreator={user?.uid === post.creatorId}
+            userVoteValue={postStateValue.userVotePost.find(i => i.postId === post.id)?.vote || 0} {...post} {...postFunction} />)}
+          {loading && <PostsSkeleton />}
+          <Box ref={ref} id="Hit this and fetch more content" />
+        </Stack>
+      </>
+      <>
+      </>
+    </ContentLayouts>
+  )
 }
