2de39fdcf7a3faf4f1d64bcc722e01e27226a88e
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-03-08 09:38:08 +0700

    feat(next.config): allow google content
    + next.config.js
    + Add 'lh3.googleusercontent.com' to domains.
    + This commit allow next to access google content.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['firebasestorage.googleapis.com']
+    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com']
   }
 }
 
