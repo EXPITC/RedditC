ba23dec75e1f3f28077ecb1d81dcaa985704ef90
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-03-09 19:56:36 +0700

    feat(homefeed): no post at subs community
    + pages/index.tsx
    + Add validation to total subs community post and if there is no
      community subs post then show the user a words.
    + This commit allow user to tell if there subs community have no post
      yet currently.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,20 @@ import ContentLayouts from "@/components/layouts/content";
 import LinkPost from "@/components/r/linkpost";
 import Post from "@/components/r/postTimeline/post";
 import PostsSkeleton from "@/components/skeleton/postSkeleton";
+import { communitySubsState } from "@/libs/atoms/communitiesAtoms";
 import useHomeFeed from "@/libs/hooks/useHomeFeed";
 import useInfoModalProps from "@/libs/hooks/useInfoModalProps";
 import usePost from "@/libs/hooks/usePosts";
-import { Stack, Box } from "@chakra-ui/react";
+import { Stack, Box, Flex, Text } from "@chakra-ui/react";
 import { useInView } from "framer-motion";
 import { useEffect, useRef } from "react";
+import { useRecoilValue } from "recoil";
 
 export default function Home() {
   const { postStateValue, loading, getNextHomeFeed, user } = useHomeFeed()
   const postFunction = usePost()
   const openModalInfoProps = useInfoModalProps()
+  const totalSubs = useRecoilValue(communitySubsState).totalSubs
 
 
   const ref = useRef<HTMLDivElement>(null)
@@ -41,6 +44,11 @@ export default function Home() {
             alreadyInComment={false}
             link={'http://localhost:3000/r/' + post.communityId + '/comments/' + post.id}
           />)}
+          {postStateValue.posts.length === 0 && totalSubs !== -1 && !loading && (
+            <Flex h={['80px', "175px"]} justify="center" align="center">
+              <Text color="gray.400" fontWeight="semibold" fontSize={["10pt", "14pt"]}>There are no posts in your community...<br />Hey look! there's a butterfly over there</Text>
+            </Flex>
+          )}
           {loading && <PostsSkeleton />}
         </Stack>
         <Box ref={ref} position="relative" bottom="150px" id="Hit this and fetch more content" />
@@ -55,6 +63,6 @@ export default function Home() {
           </Stack>
         </Stack>
       </>
-    </ContentLayouts>
+    </ContentLayouts >
   )
 }
