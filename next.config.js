aec574a0d730f1b944ea96e4bc8fbb5f8b57fa22
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-03-10 11:38:00 +0700

    set next compiler config for production

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: {
+      exclude: ['error']
+    },
+    styledComponents: {
+      minify: true
+    },
+    emotion: {
+      sourceMap: true,
+      autoLabel: 'always',
+      labelFormat: `[filename]`
+    }
+  },
+  swcMinify: true,
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com']
